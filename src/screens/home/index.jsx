27e57441a9a80d43549b8b586d379c077e2a4814
Selin ATMACA <selinatmacayy@gmail.com>
2025-02-09 19:07:13 +0300

    update

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,12 +1,19 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import Header from '../../components/home/Header';
 import Stories from '../../components/home/Stories';
+import posts from '../../utils/post';
+import Post from '../../components/home/Post';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
       <Stories />
+      <ScrollView>
+        {posts.map(post => (
+          <Post post={post} key={posts.id} />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 };
